e160632cccbe16bb51129e42177349287cd01c7c
Matthew Pombo <mppombo5@gmail.com>
2020-05-26 16:46:31 -0700

    First implementation of ArduinoController class

--- src/python/arduinoctl.py
+++ src/python/arduinoctl.py
@@ -8,6 +8,9 @@ class ArduinoController:
         self.arduino = serial.Serial(port=dev_file, baudrate=baud, timeout=5)
 
     def write_freq(self, freq: float) -> int:
-        # Python floats are represented as 64-bit double-precision floats, so pack it into 'd' for a C++ double.
-        freq_data = struct.pack('d', freq)
+        # Arduino's Serial.read returns an integer, so we need to turn the floating point frequency into an integer.
+        # We multiply the frequency by 10 and truncate the decimal points.
+        freq_int = int(freq * 10)
+
+        freq_data = struct.pack('i', freq_int)
         return self.arduino.write(freq_data)
