a982c73a48d657a0746a8de307c3c04c243666b3
Matthew Pombo <mppombo5@gmail.com>
2020-05-26 17:27:07 -0700

    Added SIGINT handler to cleanly exit detection

--- src/python/detect.py
+++ src/python/detect.py
@@ -2,6 +2,7 @@ import pyaudio
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import signal
 
 CHUNK = 4096* 4 # # data points
 RATE = 44100 # (Hz)
@@ -10,8 +11,20 @@ p=pyaudio.PyAudio()
 stream=p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,
               frames_per_buffer=CHUNK) #uses default input device
 
+# SIGINT handler to cleanly close on ctrl-C
+def close_stream(sig_num, frame):
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    exit(0)
+
+signal.signal(signal.SIGINT, close_stream)
+
 max_guitar_freq = 500 # 500 is higher than all guitar strings so ignore anything above it
 # peaks = []
+
+print("Recording ...")
+
 while True:
     data = np.fromstring(stream.read(CHUNK),dtype=np.int16)
     fourier = np.fft.fft(data)
@@ -20,11 +33,6 @@ while True:
     max_freq = max_ind * RATE / CHUNK
     print("peak: " + str(max_freq))
 #     peaks.append(max_freq)
-
-
-stream.stop_stream()
-stream.close()
-p.terminate()
 
 # plt.plot(np.arange(100), peaks)
 
